da221b43ea3d10234e6e4352ed155e8c1c703e96
ashdishgambino <anthonydesiree.pro@gmail.com>
2022-03-29 23:37:03 +0200

    MAJ CODE SQUELETTE

--- js/index.js
+++ js/index.js
@@ -1,19 +1,42 @@
-/**
- * Recuperer les produits de la page d'accueil
- */
 getProducts();
 
+/**
+ * Recupération produits API.
+ */
 
 async function getProducts (){
-    let products = await fetch('http://localhost:3000/api/products');
+    let products = await fetch("http://localhost:3000/api/products");
     console.log ("produits disponibles")
     return products.json ();
 }
 
-/**
- * Creations des produits
- */
+// Eléments du DOM que l'on souhaite afficher.
 
-async function createProduct () {
-    
-}
+async function createProducts () {
+    let result = await getProducts()
+    .then (function (APIresult){
+      const products = APIresult;
+      console.table(products);
+      for (let product in products) { 
+
+
+// Creation des balises 
+
+    let linkProduct = document.createElement ("a");
+    articleProduct.appendChild ();
+
+    let articleProduct = document.createElement ("article");
+    linkProduct.appendChild ();
+   
+    let imgProduct = document.createElement ("img");
+    articleProduct.appendChild ();
+
+    let nameProduct = document.createElement ("name");
+    imgProduct.appendChild ();
+
+    let descriptionProduct = document.createElement ("description");
+    nameProduct.appendChild ();
+  }
+
+} 
+

6da573dda260c80f328a0c5836142c484cc5b5f6
ashdishgambino <anthonydesiree.pro@gmail.com>
2022-03-29 21:32:23 +0200

    Maj Code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-fetch("http://localhost:3000/api/products");
+/**
+ * Recuperer les produits de la page d'accueil
+ */
+getProducts();
+
+/**
+ * creations des articles produits
+ */
+
+async function getProducts (){
+    let products = await fetch('http://localhost:3000/api/products');
+    console.log ("produits disponibles")
+    return products.json ();
+}
+
+async function createProduct () {
+    let 
+}
+
+
